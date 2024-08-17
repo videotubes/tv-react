889328e2aff727b99c72cb88d3fba15afe222c77
videotubes <videotubes@proton.me>
2024-08-17 15:36:56 +0700

    Update fix-dependencies.js

--- fix-dependencies.js
+++ fix-dependencies.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 const filePath = path.join(__dirname, './node_modules/@walletconnect/utils/dist/index.es.js');
 let content = fs.readFileSync(filePath, 'utf8');
-content = content.replace(/import\s+Bo/g, 'import');
+content = content.replace(/import\s+Bo,/g, 'import');
 content = content.replace(/t\s*=\s*Bo/g, 't=Ei');
 fs.writeFileSync(filePath, content, 'utf8');
