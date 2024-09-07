9548f3a4a5a09062b41744d91e25005ebe2f08c5
videotubes <videotubes@proton.me>
2024-09-08 00:43:01 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -18,7 +18,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function (event) {
   if (event.request.url.startsWith('/api/')) {
-    return; // Bypass API requests
+    return;
   }
   else {
     return new Response('Offline content not available.', { status: 404, statusText: 'Not Found' });
@@ -39,50 +39,3 @@ self.addEventListener('activate', function(event) {
     .then(() => self.clients.claim())
   )
 })
-
-/* self.addEventListener('fetch', function (event) {
-  if(typeof window !== 'undefined') {
-    if(window.matchMedia('(display-mode: standalone)').matches || navigator.standalone) {
-      if(event.request.method !== 'GET') {
-        event.respondWith(
-          fetch(event.request)
-          .catch(() => {
-            return new Response('Non-GET request cannot be cached offline.');
-          })
-        );
-      }
-      else if(event.request.url.includes('/api/')) {
-        return fetch(event.request);
-      }
-      else {
-        event.respondWith(
-          fetch(event.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-            .then((cache) => {
-              return cache.match(event.request) || new Response('Offline content not available.');
-            });
-          })
-        );
-      }
-    }
-    else {
-      return;
-    }
-  }
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys()
-    .then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if(key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Delete old cache', key)
-          return caches.delete(key)
-        }
-      }))
-    })
-    .then(() => self.clients.claim())
-  )
-}) */
