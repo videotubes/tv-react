0df4570e5673e362cfd1012b33a4a8909cb639d6
videotubes <videotubes@proton.me>
2024-09-07 15:55:23 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,88 +1,88 @@
 const CACHE_NAME = 'tvideos-pwa-cache-v1';
 const toCache = [
-	'/',
-	'/manifest.json',
+  '/',
+  '/manifest.json',
 ];
 
 self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			return cache.addAll(toCache)
-		})
-		.then(function () {
-			return self.skipWaiting();
-		})
-	)
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(toCache)
+    })
+    .then(function () {
+      return self.skipWaiting();
+    })
+  )
 })
 
 self.addEventListener('fetch', function (event) {
-	if (event.request.url.startsWith('/api/')) {
-		return; // Bypass API requests
-	}
-	else {
-		return new Response('Offline content not available.', { status: 404, statusText: 'Not Found' });
-	}
+  if (event.request.url.startsWith('/api/')) {
+    return; // Bypass API requests
+  }
+  else {
+    return new Response('Offline content not available.', { status: 404, statusText: 'Not Found' });
+  }
 });
 
 self.addEventListener('activate', function(event) {
-	event.waitUntil(
-		caches.keys()
-		.then((keyList) => {
-			return Promise.all(keyList.map((key) => {
-				if (key !== CACHE_NAME) {
-					console.log('[ServiceWorker] Delete old cache', key)
-					return caches.delete(key)
-				}
-			}))
-		})
-		.then(() => self.clients.claim())
-	)
+  event.waitUntil(
+    caches.keys()
+    .then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Delete old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+    .then(() => self.clients.claim())
+  )
 })
 
 /* self.addEventListener('fetch', function (event) {
-	if(typeof window !== 'undefined') {
-		if(window.matchMedia('(display-mode: standalone)').matches || navigator.standalone) {
-			if(event.request.method !== 'GET') {
-				event.respondWith(
-					fetch(event.request)
-					.catch(() => {
-						return new Response('Non-GET request cannot be cached offline.');
-					})
-				);
-			}
-			else if(event.request.url.includes('/api/')) {
-				return fetch(event.request);
-			}
-			else {
-				event.respondWith(
-					fetch(event.request)
-					.catch(() => {
-						return caches.open(CACHE_NAME)
-						.then((cache) => {
-							return cache.match(event.request) || new Response('Offline content not available.');
-						});
-					})
-				);
-			}
-		}
-		else {
-			return;
-		}
-	}
+  if(typeof window !== 'undefined') {
+    if(window.matchMedia('(display-mode: standalone)').matches || navigator.standalone) {
+      if(event.request.method !== 'GET') {
+        event.respondWith(
+          fetch(event.request)
+          .catch(() => {
+            return new Response('Non-GET request cannot be cached offline.');
+          })
+        );
+      }
+      else if(event.request.url.includes('/api/')) {
+        return fetch(event.request);
+      }
+      else {
+        event.respondWith(
+          fetch(event.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+            .then((cache) => {
+              return cache.match(event.request) || new Response('Offline content not available.');
+            });
+          })
+        );
+      }
+    }
+    else {
+      return;
+    }
+  }
 });
 
 self.addEventListener('activate', function(event) {
-	event.waitUntil(
-		caches.keys()
-		.then((keyList) => {
-			return Promise.all(keyList.map((key) => {
-				if(key !== CACHE_NAME) {
-					console.log('[ServiceWorker] Delete old cache', key)
-					return caches.delete(key)
-				}
-			}))
-		})
-		.then(() => self.clients.claim())
-	)
+  event.waitUntil(
+    caches.keys()
+    .then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if(key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Delete old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+    .then(() => self.clients.claim())
+  )
 }) */
