af3caf8d3a9b816bec3538d661d3005c916d389d
videotubes <videotubes@proton.me>
2024-09-07 15:45:08 +0700

    Update FetchMore.js

--- src/components/FetchMore.js
+++ src/components/FetchMore.js
@@ -1,14 +1,14 @@
 // Fetch more videos, used and trigger by Home page component
 const FetchMore = async (apiUrl) => {
-	try {
-		const response = await fetch(apiUrl);
-		const jsonData = await response.json();
-		return jsonData;
-	}
-	catch (error) {
-		console.error(`Error fetching data from ${apiUrl}:`, error);
-		throw error;
-	}
+  try {
+    const response = await fetch(apiUrl);
+    const jsonData = await response.json();
+    return jsonData;
+  }
+  catch (error) {
+    console.error(`Error fetching data from ${apiUrl}:`, error);
+    throw error;
+  }
 };
 
 module.exports = FetchMore;
