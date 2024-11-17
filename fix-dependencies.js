8c0fac42b4cb2ec59e5350db103b368bb3caa616
videotubes <videotubes@proton.me>
2024-11-17 13:56:37 +0700

    Delete fix-dependencies.js

--- fix-dependencies.js
+++
@@ -1,9 +0,0 @@
-// fix "Bo"
-const fs = require('fs');
-const path = require('path');
-
-const filePath = path.join(__dirname, './node_modules/@walletconnect/utils/dist/index.es.js');
-let content = fs.readFileSync(filePath, 'utf8');
-content = content.replace(/import\s+Bo,/g, 'import');
-content = content.replace(/t\s*=\s*Bo/g, 't=Ei');
-fs.writeFileSync(filePath, content, 'utf8');
