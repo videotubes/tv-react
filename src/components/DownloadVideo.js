c7376caab630876178ab6d25d111202549f30b40
videotubes <videotubes@proton.me>
2024-11-20 01:31:31 +0700

    fix: pop up not open on mobile

--- src/components/DownloadVideo.js
+++ src/components/DownloadVideo.js
@@ -4,30 +4,27 @@ const DownloadVideo = ({ videoUrl, buttonName }) => {
   
   const shortLinkUrl = process.env.REACT_APP_SHORTENER_URL_ENDPOINT;
   
-  // Detect if is on mobile for download popup
-  function isMobileDevice() {
-    return /Mobi|Android/i.test(navigator.userAgent);
-  }
-  
   // Download video if video can downloaded (not live stream) if else it will redirect to user room, before that this will generate to short link url.
   const downloadPopUp = async (e) => {
     const longUrl = e.target.value;
     const url = `${shortLinkUrl}&url=${longUrl}`;
-    const responseData = await fetch(url);
-    const shortedUrl = await responseData.json();
-    if (shortedUrl.success === true) {
-      if (isMobileDevice()) {
-        const openInNewTab = window.confirm("Open the link in a new tab?");
-        if (openInNewTab) {
-          window.open(shortedUrl.short_url, '_blank');
+    const newTab = window.open("about:blank", "_blank");
+    try {
+      const responseData = await fetch(url);
+      if (responseData.ok) {
+        let targetUrl;
+        const shortedUrl = await responseData.json();
+        if (shortedUrl.success === true) {
+          targetUrl = shortedUrl.short_url;
         }
-        else {
-          window.location.href = shortedUrl.short_url;
-        }
+        newTab.location.href = targetUrl;
+      } else {
+        newTab.close();
+        return;
       }
-      else {
-        window.open(shortedUrl.short_url, '_blank');
-      }
+    } catch (error) {
+      newTab.close();
+      console.error(error);
     }
   };
   
