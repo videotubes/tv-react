84339d99a6b4500728c77aa00f6354f84baa2d81
videotubes <videotubes@proton.me>
2024-12-24 15:59:32 +0700

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import LazyLoad from 'react-lazyload';
 
 // Component for check each image for video VideoThumbnail list spesific for compilations. Video image at compilations sometime missing so it will fallback to spesific image for best user experience. This trigger by VideoThumbnail page component
-const Image = ({ src, alt, title, key, getFallbackSrc }) => {
+const Image = ({ src, alt, title, getFallbackSrc }) => {
   const [fallbackIndex, setFallbackIndex] = useState(0);
   const [imageKey, setImageKey] = useState(Date.now());
 
@@ -20,7 +20,7 @@ const Image = ({ src, alt, title, key, getFallbackSrc }) => {
   }, [src])
   
   return (
-    <LazyLoad key={key} placeholder={<img src="/assets/tubevideos_placeholder.webp" />}>
+    <LazyLoad key={imageKey} placeholder={<img src="/assets/tubevideos_placeholder.webp" />}>
       <img
         src={src}
         alt={alt}
